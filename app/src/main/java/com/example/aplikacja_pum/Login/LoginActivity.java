1f3999ec791567db757330d48e759c04d5a31688
kkuzmicki <kkuzmicki@wp.pl>
2020-08-17 23:48:40 +0200

    odpalanie ekranu logowania

--- app/src/main/java/com/example/aplikacja_pum/Login/LoginActivity.java
+++ app/src/main/java/com/example/aplikacja_pum/Login/LoginActivity.java
@@ -2,6 +2,9 @@ package com.example.aplikacja_pum.Login;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,11 +14,25 @@ public class LoginActivity extends AppCompatActivity
 {
     private static final String TAG = "LoginActivity";
 
+    private ProgressBar loginPB;
+    private EditText emailET;
+    private EditText passwordET;
+    private TextView pleaseWaitTV;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        init();
         Log.d(TAG, "Ekran logowania!");
+    }
+
+    private void init()
+    {
+        //loginPB = findViewById(R.id.)
+        emailET = findViewById(R.id.emailET);
+        passwordET = findViewById(R.id.passwordET);
+
     }
 }
