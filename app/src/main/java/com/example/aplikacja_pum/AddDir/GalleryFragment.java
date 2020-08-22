01ab43332bf5cbacd8bc9fd5c05d931e2f0711b2
Mateusz <raczkamateusz7@gmail.com>
2020-08-22 20:52:46 +0200

    Mateusz

--- app/src/main/java/com/example/aplikacja_pum/AddDir/GalleryFragment.java
+++ app/src/main/java/com/example/aplikacja_pum/AddDir/GalleryFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
@@ -15,6 +17,10 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.aplikacja_pum.R;
+import com.example.aplikacja_pum.Utils.FilePaths;
+import com.example.aplikacja_pum.Utils.FileSearch;
+
+import java.util.ArrayList;
 
 public class GalleryFragment extends Fragment {
 
@@ -22,6 +28,7 @@ public class GalleryFragment extends Fragment {
     private Spinner directorySpinner;
     private GridView gridView;
     private ImageView galleryImage;
+    private ArrayList<String> list;
 
     @Nullable
     @Override
@@ -34,6 +41,7 @@ public class GalleryFragment extends Fragment {
         directorySpinner = (Spinner) view.findViewById(R.id.spinnerDirectory);
         mProgressBar = (ProgressBar) view.findViewById(R.id.progressBar);
         mProgressBar.setVisibility(View.GONE);
+        list = new ArrayList<>();
 
         ImageView  imageView = (ImageView) view.findViewById(R.id.ivClose);
         imageView.setOnClickListener(new View.OnClickListener() {
@@ -43,6 +51,41 @@ public class GalleryFragment extends Fragment {
             }
         });
 
+        TextView textView = (TextView) view.findViewById(R.id.tvNext);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        init();
         return view;
     }
+
+    private void init(){
+        //wybor folderu
+        FilePaths filePaths = new FilePaths();
+
+        if(FileSearch.getDirectoryPaths(filePaths.PICTURES) != null){
+            list = FileSearch.getDirectoryPaths(filePaths.PICTURES);
+        }
+        //list.add(filePaths.CAMERA);
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_spinner_item,list);
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        directorySpinner.setAdapter(arrayAdapter);
+        directorySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+
 }
---
+++ app/src/main/java/com/example/aplikacja_pum/Utils/FilePaths.java
@@ -0,0 +1,10 @@
+package com.example.aplikacja_pum.Utils;
+
+import android.os.Environment;
+
+public class FilePaths {
+
+    public String ROOT_DIR = Environment.getExternalStorageDirectory().getPath();
+    public String PICTURES = ROOT_DIR + "/Pictures";
+    public String CAMERA = ROOT_DIR + "/DCIM/camera";
+}
